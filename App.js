96ecfb69caff8dd07efd0ee2f8a64d50c89a09e1
ja1c <marinas.ezra43@gmail.com>
2024-11-05 12:45:04 +0800

    minor changes

--- App.js
+++ App.js
@@ -13,10 +13,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Log In" component={LoginScreen} />
-        <Stack.Screen name="Sign Up" component={SignupScreen} />
-        <Stack.Screen name="User Profile" component={UserProfile} />
-        <Stack.Screen name="Password Recovery" component={PasswordRecoveryScreen} />
+        <Stack.Screen name="Log In" component={LoginScreen} 
+        options={{headerShown:false}}/>
+        <Stack.Screen name="Sign Up" component={SignupScreen}
+        options={{headerShown:false}}/>
+        <Stack.Screen name="User Profile" component={UserProfile} 
+        options={{headerShown:false}}/>
+        <Stack.Screen name="Password Recovery" component={PasswordRecoveryScreen}
+        options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
