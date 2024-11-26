8999e062f1ea1fce1da4df8c9979f57c78a3932e
ja1c <marinas.ezra43@gmail.com>
2024-11-26 20:11:47 +0800

    Drawer

--- app/Dashboard/_layout.js
+++ app/Dashboard/_layout.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 
@@ -6,31 +5,12 @@ export default function DrawerLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer>
-        {/* Home Screen */}
-        <Drawer.Screen
-          name="index" // This corresponds to the Home screen (index.js)
+      <Drawer.Screen
+          name="(tabs)" // This is the name of the page and must match the url from root
           options={{
-            drawerLabel: 'Home',  // Label displayed in the drawer
-            title: 'Overview',    // Title for the screen
-          }}
-        />
-
-        {/* Add more screens to the drawer if needed */}
-        <Drawer.Screen
-          name="UserProfile"  // Corresponds to the UserProfile screen (UserProfile.js)
-          options={{
-            drawerLabel: 'Profile',
-            title: 'User Profile',
-          }}
-        />
-
-        <Drawer.Screen
-          name="Settings"  // Corresponds to the Settings screen (Settings.js)
-          options={{
-            drawerLabel: 'Settings',
-            title: 'Settings',
-          }}
-        />
+            drawerLabel: 'Home',
+            title: 'Home',
+          }}/>
       </Drawer>
     </GestureHandlerRootView>
   );
